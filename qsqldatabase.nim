fa75e51216a687a418fd45eb302d1defec5c3373
Philip Wernersbach <philip.wernersbach@gmail.com>
2015-10-19 11:36:22 -0400

    Use correct case for QSqlDatabase header.

--- qsqldatabase.nim
+++ qsqldatabase.nim
@@ -1,5 +1,5 @@
 import qt5_qtsql
 
-const QSQLDATABASE_H = "<QtSql/QSQlDatabase>"
+const QSQLDATABASE_H = "<QtSql/QSqlDatabase>"
 
 proc qSqlDatabaseRemoveDatabase*(connectionName: cstring) {.header: QSQLDATABASE_H, importcpp: "QSqlDatabase::removeDatabase(@)".}
